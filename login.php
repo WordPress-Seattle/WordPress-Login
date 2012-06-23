64dba28bbdb98930adbf6535ba7704dc0cea22a3
Jaffe <jaffe75@gmail.com>
2012-06-23 13:25:56 -0700

    Rolling
    I figured I would get the ball rolling…

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Wordpress Login
+Description: Allows users to login to multiple Wordpress sites from a single Wordpress installation.
+Version: 1.0
+Author: WPSeattle
+*/
+
+
