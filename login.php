b6ca1acb86cbe2b3095a9cb6917db9a562019100
Jaffe <jaffe75@gmail.com>
2012-06-23 19:29:22 -0700

    text change
    changed text in Plugin desc to match README

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: Wordpress Login
-Description: Allows users to login to multiple Wordpress sites from a single Wordpress installation.
+Plugin Name: Wordpress-Login
+Description: An easy to use authentication system to authenticate WordPress users via a remote WordPress installation
 Version: 1.0
 Author: WPSeattle
 */
