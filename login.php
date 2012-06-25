f629f92d8e5c1dd1759e8adbfb214b810b639b6d
Jaffe <jaffe75@gmail.com>
2012-06-25 10:39:53 -0700

    Delete Login.php file

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: Wordpress-Login
-Description: An easy to use authentication system to authenticate WordPress users via a remote WordPress installation
-Version: 1.0
-Author: WPSeattle
-*/
-
-
